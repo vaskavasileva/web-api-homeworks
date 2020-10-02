b5a06cec7927d6a781a6ea5394b7d4b48b9d1f8d
vaskavasileva <61829894+vaskavasileva@users.noreply.github.com>
2020-10-02 13:52:15 +0200

    Homework

---
+++ SEDC.MyProject/SEDC.MyProject/Controllers/UsersController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.MyProject.Models;
+
+namespace SEDC.MyProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<User>> GetUsers()
+        {
+            return Ok(StaticDb.AllUsers);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<User> GetUserById(int id)
+        {
+            var user = StaticDb.AllUsers.SingleOrDefault(u => u.Id == id);
+            if (id < 1)
+            {
+                return BadRequest(null);
+            }
+            if (user == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, null);
+            }
+            return Ok(user);
+        }
+        [HttpPost]
+        public ActionResult AddUser([FromBody] User user)
+        {
+            user.Id = StaticDb.AllUsers.Count + 1;
+            StaticDb.AllUsers.Add(user);
+            return Ok();
+        }
+        [HttpDelete]
+        public ActionResult DeleteUser(int id)
+        {
+            var userToDelete = StaticDb.AllUsers.SingleOrDefault(u => u.Id == id);
+            if (id < 1)
+            {
+                return BadRequest();
+            }
+            if (userToDelete == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+            StaticDb.AllUsers.Remove(userToDelete);
+            return Ok();
+        }
+    }
+}
---
+++ SEDC.MyProject/SEDC.MyProject/StaticDb.cs
@@ -0,0 +1,51 @@
+﻿using SEDC.MyProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.MyProject
+{
+    public static class StaticDb
+    {
+        public static List<User> AllUsers = new List<User>()
+        {
+            new User()
+            {
+                Id = 1,
+                FirstName = "Vaska",
+                LastName = "Vasileva"
+            },
+            new User()
+            {
+                Id = 2,
+                FirstName = "Goce",
+                LastName = "Kabov"
+            },
+            new User()
+            {
+                Id = 3,
+                FirstName = "Darko",
+                LastName = "Panchevski"
+            },
+            new User()
+            {
+                Id = 4,
+                FirstName = "Matej",
+                LastName = "Jovanov"
+            },
+            new User()
+            {
+                Id = 5,
+                FirstName = "Tea",
+                LastName = "Stojkovska"
+            },
+            new User()
+            {
+                Id = 6,
+                FirstName = "Jovana",
+                LastName = "Jovanovska"
+            },
+        };
+    }
+}
